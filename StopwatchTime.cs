d4e1f6085f34368c94af735dc84d9cbb47bad971
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-14 08:57:23 +0530

    [Raj]Add: To Check elapsed time with Stopwatch

---
+++ StopwatchTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LogicalProgram
+{
+    internal class StopwatchTime
+    {
+        public void ElapsedTime()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Thread.Sleep(15600);
+            watch.Stop();
+            TimeSpan tspan = watch.Elapsed;
+            string elapsedTime = String.Format(" {0:00}:{1:00}:{2:00}.{3:00} ",
+            tspan.Hours, tspan.Minutes, tspan.Seconds, tspan.Milliseconds / 10);
+            Console.WriteLine(" RunTime " + elapsedTime);
+        }
+    }
+}
