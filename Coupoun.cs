fda7f8827133e8c92d5b3509c010946a72f5fb6e
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-14 08:55:19 +0530

    [Raj]Add: To generate coupouns

---
+++ Coupoun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class Coupoun
+    {
+        public void CoupounNumber()
+        {
+            Console.Write("Enter the numbers of coupouns you want ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Random random = new Random();
+                Console.Write("\n" + random.Next(100, 1000));   
+            }
+        }
+    }
+}
