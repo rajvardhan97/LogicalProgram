3a3517ea322b6276258e12aefb473102686a0733
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-14 08:03:48 +0530

    [Raj]Add: To reverse a number

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class ReverseNumber
+    {
+        public void Reverse()
+        {
+            Console.Write("Enter a number to reverse it "); 
+            int number = Convert.ToInt32(Console.ReadLine());
+			int reverse = 0;
+
+			while (number > 0)				
+			{
+				reverse = reverse * 10 + number % 10;		
+				number = number / 10;
+			}
+
+			Console.WriteLine("Reversed number: " + reverse);
+		}
+    }
+}
