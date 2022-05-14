d198fe1bca9cbacba4b8e257098bf1e9aaeff6a1
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-14 08:01:01 +0530

    [Raj]Add: To check if a number is prime number

---
+++ PrimeNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class PrimeNumber
+    {
+        public void Prime()
+        {
+            Console.WriteLine("Enter a number to check if it is prime ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(number + " is not a prime number");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(number + " is a prime number");
+                }
+            }
+        }
+    }
+}
